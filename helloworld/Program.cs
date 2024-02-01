219be4ddc66144fe3371ae29454cc8c6f8d9280c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-01 15:49:33 -0500

    Add exercise

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,1 +1,18 @@
 ﻿Console.WriteLine("Im learning C#");
+
+// program that calculates the area of ​​a rectangle
+
+double sidea;
+double sideb;
+double result;
+
+Console.WriteLine("\nCalculate the area of ​​a rectangle!!");
+
+Console.WriteLine("Insert value A: ");
+sidea = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insert value B: ");
+sideb = Convert.ToDouble(Console.ReadLine());
+
+result = sidea * sideb;
+Console.WriteLine("\nSide A: " + sidea + "\tSide B: " + sideb + "\nThe area of reactangle is: " + result);
